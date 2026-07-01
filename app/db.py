d6448f75fa24389546e4325618bb860973fd13cb
professorLumpen <jeanpaulvsratyi@gmail.com>
2026-07-01 19:21:32 +0300

    Added config and db_settings

---
+++ app/db.py
@@ -0,0 +1,17 @@
+from sqlalchemy.ext.asyncio import create_async_engine, async_sessionmaker
+from sqlalchemy.orm import DeclarativeBase
+
+from app.config import settings
+
+
+class Base(DeclarativeBase):
+    pass
+
+
+engine = create_async_engine(settings.db_url)
+session_maker = async_sessionmaker(engine)
+
+
+async def get_session():
+    async with session_maker() as session:
+        yield session
