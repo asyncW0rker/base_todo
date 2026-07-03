8c8ef206e1acddd0516fa1d2ec42952dd5545032
professorLumpen <jeanpaulvsratyi@gmail.com>
2026-07-03 19:50:39 +0300

    Updated models - add relatioship for User and ToDo

--- app/database/models.py
+++ app/database/models.py
@@ -1,4 +1,5 @@
-from sqlalchemy.orm import Mapped, mapped_column
+from sqlalchemy import ForeignKey
+from sqlalchemy.orm import Mapped, mapped_column, relationship
 
 from app.database.db import Base
 
@@ -10,6 +11,8 @@ class User(Base):
     username: Mapped[str]
     password: Mapped[str]
 
+    todos: Mapped[list["ToDo"]] = relationship(back_populates="user")
+
 
 class ToDo(Base):
     __tablename__ = "todos"
@@ -18,3 +21,6 @@ class ToDo(Base):
     title: Mapped[str]
     description: Mapped[str]
     completed: Mapped[bool] = mapped_column(default=False)
+
+    user_id: Mapped[int | None] = mapped_column(ForeignKey(User.id, ondelete="CASCADE"), nullable=True)
+    user: Mapped[User] = relationship(back_populates="todos")
