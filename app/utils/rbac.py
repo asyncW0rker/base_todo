265c868a080c3e7cb48b7bebe04ad2ab2f568ffc
professorLumpen <jeanpaulvsratyi@gmail.com>
2026-07-24 16:49:02 +0300

    Fixed RBAC permissions

--- app/utils/rbac.py
+++ app/utils/rbac.py
@@ -6,17 +6,14 @@ from app.services.auth_service import AuthService, get_auth_service
 from app.utils.security import JWTManager, oauth_scheme
 
 
-def admin_permission_required(token: str = Depends(oauth_scheme), jwt_manager: JWTManager=Depends()):
-    pass
-
-
 def role_permission_required(required_role: UserRole = UserRole.ADMIN):
     def check_permission(
         token: str = Depends(oauth_scheme),
         auth_service: AuthService = Depends(get_auth_service),
     ):
         payload = auth_service.decode_access_token(token)
-        if payload['role'] not in [required_role, UserRole.ADMIN]:
+        user_role = payload.get("role")
+        if user_role not in [required_role, UserRole.ADMIN]:
             raise HTTPRolePermissionDeniedException
 
     return check_permission
