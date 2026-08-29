c466c4d6ea59f68042a31dc14244e72d05859fbe
professorLumpen <jeanpaulvsratyi@gmail.com>
2026-08-29 19:06:01 +0300

    Created dockerfile for worker

--- app/utils/background/broker.py
+++ app/utils/background/broker.py
@@ -3,6 +3,10 @@ from taskiq_redis import ListQueueBroker, RedisAsyncResultBackend
 from app.utils.config import settings
 
 
-broker = ListQueueBroker(url=settings.redis.redis_url).with_result_backend(
+broker = ListQueueBroker(
+    url=settings.redis.redis_url,
+    socket_timeout=None,
+    socket_connect_timeout=10,
+).with_result_backend(
     RedisAsyncResultBackend(settings.redis.redis_url)
 )
