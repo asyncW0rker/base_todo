491247f16fe3340b81c28cd5ecf25f32277225aa
professorLumpen <jeanpaulvsratyi@gmail.com>
2026-09-11 21:11:41 +0300

    Added get_db_context with rollback

--- app/database/db.py
+++ app/database/db.py
@@ -1,3 +1,5 @@
+from contextlib import asynccontextmanager
+
 from sqlalchemy.ext.asyncio import create_async_engine, async_sessionmaker
 from sqlalchemy.orm import DeclarativeBase
 
@@ -12,6 +14,18 @@ engine = create_async_engine(settings.database.db_url)
 session_maker = async_sessionmaker(engine, expire_on_commit=False)
 
 
-async def get_session():
+@asynccontextmanager
+async def get_db_context():
     async with session_maker() as session:
+        try:
+            yield session
+        except Exception:
+            await session.rollback()
+            raise
+        finally:
+            await session.close()
+
+
+async def get_session():
+    async with get_db_context() as session:
         yield session
